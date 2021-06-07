dca73e2aaf23058a854ebe97ff2ac1894dbf8398
Daniel Verdú <verdudaniel7@gmail.com>
2021-06-07 20:33:12 +0200

    seachbar funcionalidad completada

--- src/components/Patients/PatientList.vue
+++ src/components/Patients/PatientList.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
+    <b-container>
+      <b-row>
+        <b-col>
+
+        </b-col>
+        <b-col>
+          <search-bar
+          @SearchItem="inptValue = $event"
+          >
+          </search-bar>
+        </b-col>
+      </b-row>
+    </b-container>
       <b-container>
           <b-row v-if="getPatientList">
             <single-patient
-                v-for="(patient, index) of getPatientList" 
+                v-for="(patient, index) of filterPatient" 
                 :key="index"
                 :patient="patient"
                 :viewOpt="viewOpt"
@@ -15,16 +28,22 @@
 </template>
 
 <script>
+import SearchBar from '../UI/SearchBar.vue'
 import SinglePatient from './SinglePatient.vue'
 export default {
-  components: { SinglePatient },
+  components: { SinglePatient, SearchBar },
     name: 'PatientList',
+    data(){
+      return{
+        inptValue: ''
+      }
+    },
     computed:{
         filterPatient(){
         return this.getPatientList.filter((patient) => {
           return (
-            patient.datos_paciente.nombre.toLowerCase().match(this.searchPatient.toLowerCase().trim()) ||
-            patient.datos_paciente.apellidos.toLowerCase().match(this.searchPatient.toLowerCase().trim())
+            patient.datos_paciente.nombre.toLowerCase().match(this.inptValue.toLowerCase().trim()) ||
+            patient.datos_paciente.apellidos.toLowerCase().match(this.inptValue.toLowerCase().trim())
           )
         })
       },
---
+++ src/components/UI/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search-bar">
+      <input 
+        type="text"
+        v-model="search"
+        @keyup="seachItem($event)"
+        >
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    data(){
+      return{
+        search: ''
+      }
+    },
+    methods:{
+      seachItem(){
+        this.$emit('SearchItem', this.search)
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
