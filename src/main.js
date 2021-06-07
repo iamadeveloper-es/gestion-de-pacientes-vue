c73729eb505d5d5a9a0f2c03e06374d0b3c60c1b
Daniel Verdú <verdudaniel7@gmail.com>
2021-06-07 19:56:20 +0200

    configuración proyecto

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { BootstrapVue} from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
